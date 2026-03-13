4c18ef2f94f4af6697d61fb4d7b725b45d3ad23c
Poojanirmal2003-bit <poojanirmal2003@gmail.com>
2026-03-13 18:54:36 +0530

    hw

---
+++ SumofTwo.java
@@ -0,0 +1,13 @@
+class SumofTwo
+{
+	public static void main(String[] args)
+	{
+		int ar [] ={ 4 , 6 ,10};
+		int sum = ar[0]+ ar[1];
+		
+		{
+			System.out.println("The sum is:"+sum);
+		}
+		
+	}
+}
